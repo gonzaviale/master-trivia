1a085a29de1eb7e86d1c5f7c45c908222af57145
gonzaviale <gonzaloleonelviale@gmail.com>
2024-06-26 18:43:09 -0300

    ..

--- controller/RegistroController.php
+++ controller/RegistroController.php
@@ -155,7 +155,6 @@ class RegistroController
 
 
             $mail->send();
-            echo 'Message has been sent';
         } catch (Exception $e) {
             echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
         }
